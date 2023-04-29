d93b505e8245ab231d263bee8ff0e2b6f7f2c47d
Sajal Srivastava <sajalsrivastava123123aa@gmail.com>
2023-04-29 10:01:18 +0530

    post commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 const {connectDB} = require('./db/connect');
 const project = require('./routes/project');
 const port = process.env.PORT || 8000;
--- server/models/project.js
+++ server/models/project.js
@@ -13,7 +13,7 @@ const Project = mongoose.Schema({
 
   balance: {
     type: String,
-    required: [true, "a project title must be provided"],
+    //required: [true, "a project title must be provided"],
   },
 
   walletaddress: {
@@ -23,7 +23,7 @@ const Project = mongoose.Schema({
 
   financeValidatoraddress: {
     type: String,
-    required: [true],
+    //required: [true],
   },
 
   balance: {
@@ -38,7 +38,7 @@ const Project = mongoose.Schema({
 
   contractAddress: {
     type: Number,
-    required: [true],
+    //required: [true],
   },
 
   status: {
